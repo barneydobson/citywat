a5b2f8cf96003c10ef7df01d9afc009622eba9b8
barneydobson <barnaby.dobson1@gmail.com>
2020-03-09 15:43:18 +0000

    Update options.py
    Updated numbers following discussion with Tom

--- scripts/options.py
+++ scripts/options.py
@@ -20,9 +20,9 @@ def add_option(model,names):
         elif (name == 'effluent_reuse'):
             model.parameters['effluent_reuse_capacity'] += 150
         elif (name == 'demand_reduction'):
-            model.parameters['per_household_consumption'] *= 0.9
+            model.parameters['per_household_consumption'] *= 0.9 # Achieved by increased metering - 10% achieveable by about 2035
         elif (name == 'leakage'):
-            model.parameters['distribution_leakage'] *= 0.5
+            model.parameters['distribution_leakage'] *= 0.65
         elif (name == 'green_roofs_equivalent'):
             #Calculate as equivalent greenspace increase
             area = 3
@@ -38,14 +38,16 @@ def add_option(model,names):
 #            attenuation_capacity = 200
 #            model.parameters['impermeable_surface_storage_capacity'] += (area * attenuation_capacity * constants.MM_KM2_TO_ML)
         elif (name == 'water_butts'):
-            model.parameters['rainwater_harvesting_penetration'] = 18 # % (of households and of roof area)
-            model.parameters['rainwater_harvesting_storage_capacity'] = 240 # Ml (assuming a 400L water butt for 18% of London's households)
-        elif (name == 'wwtw_rate'):
-            model.parameters['wastewater_treatment_max_rate_change'] *= 1.1
-        elif (name == 'wwtw_cap'):
-            model.parameters['wastewater_treatment_plant_maximum_capacity'] *= 1.1
+            model.parameters['rainwater_harvesting_penetration'] = 100 # % (of households and of roof area)
+            model.parameters['rainwater_harvesting_storage_capacity'] = 280 # Ml (assuming a 400L water butt for 18% of London's households)
+#        elif (name == 'wwtw_rate'):
+#            model.parameters['wastewater_treatment_max_rate_change'] *= 1.1
+#        elif (name == 'wwtw_cap'):
+#            model.parameters['wastewater_treatment_plant_maximum_capacity'] *= 1.1
         elif (name == 'wwtw_storage'):
-            model.parameters['wastewater_temporary_storage_capacity'] *= 1.1
+            model.parameters['wastewater_temporary_storage_capacity'] += 150
+        else:
+            print('option not found')
 
 def options_list():
     return ['nopump_rule',
@@ -56,6 +58,6 @@ def options_list():
             'green_roofs_equivalent',
 #            'green_roofs_storage',
             'water_butts',
-            'wwtw_rate',
-            'wwtw_cap',
+#            'wwtw_rate',
+#            'wwtw_cap',
             'wwtw_storage']
